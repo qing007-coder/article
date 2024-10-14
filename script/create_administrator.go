52d6a8ae894624ba5a2243ceff4ed8210513249c
MountQing <13767463+mountqing@user.noreply.gitee.com>
2024-10-14 12:47:37 +0800

    coding script

--- script/create_administrator.go
+++ script/create_administrator.go
@@ -1,12 +1,22 @@
 package script
 
 import (
+	"article/pkg/constant"
+	"article/pkg/model"
 	"article/pkg/rules"
 	"article/pkg/tools"
+	"gorm.io/gorm"
 )
 
-func CreateAdministrator(enforcer *rules.Enforcer) {
+func CreateAdministrator(db *gorm.DB, enforcer *rules.Enforcer) error {
 	id := tools.CreateID()
 
-	enforcer.AddGroup()
+	db.Create(&model.User{
+		ID:       id,
+		Account:  "1457800511",
+		Password: "qing040824",
+		Email:    "1457800511@qq.com",
+		Status:   true,
+	})
+	return enforcer.AddGroup(id, constant.ADMINISTRATOR)
 }
